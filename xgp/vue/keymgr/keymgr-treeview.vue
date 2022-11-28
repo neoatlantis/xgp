f5abbd2ff9f9663cfc47cfb8a0c18e193b3141e5
NeoAtlantis <contact@chaobai.li>
2022-11-28 23:11:37 +0100

    save

--- xgp/vue/keymgr/keymgr-treeview.vue
+++ xgp/vue/keymgr/keymgr-treeview.vue
@@ -10,9 +10,9 @@
         <summary>🔑 Private Keys</summary>
         <ul>
           <li><i>Import a new private key...</i></li>
-          <li><i>Generate a new keypair...</i></li>
         </ul>
     </details></li>
+    <li><i>Generate a new keypair...</i></li>
 </ul>
 </template>
 <script>
